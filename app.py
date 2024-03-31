cded654be861fc232a5d47834e43fe00d6e46d65
StanimirVasev <s.vasev97@gmail.com>
2024-03-31 01:48:27 +0000

    Add profile section

--- app.py
+++ app.py
@@ -55,6 +55,8 @@ def register():
         # Put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("You account has been created successfully!", 'flash-message--success')
+        return redirect(url_for(
+            "profile", username=session["user"]))
     return render_template("register.html")
 
 
@@ -71,7 +73,8 @@ def login():
                     existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Hello, {}".format(request.form.get("username")), 'flash-message--success')
-                return redirect(url_for("profile", username=session["user"]))
+                return redirect(url_for(
+                    "profile", username=session["user"]))
             else:
                 # Show error message if incorrect password is used
                 flash("Incorrect password. Please try again.", 'flash-message--error')
@@ -83,6 +86,14 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    # Get user's username from db
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
